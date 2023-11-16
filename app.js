c04af288180a14b3bb95fd58aca5a763248a574e
shabeera <shabeerashaik10@gmail.com>
2023-11-16 17:23:30 +0530

    small project

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
-const heading = React.createElement("h1",{id:"heading"}, "creating a heading")
-const heading2 = React.createElement("h2",{id:"title"}, "creating a heading2 ")
+import ReactDOM from "react-dom/client";
+import userlogin from "./userlogin.png";
+import react from "./react.png";
+import "./index.css";
 
-const Container = React.createElement("div", {id:"container"}, [heading,heading2])
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(Container)
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="logo-container">
+        <img src={react} alt="Logo" className="logo" />
+      </div>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search..."
+          aria-label="Search"
+          className="search-input"
+        />
+      </div>
+      <div className="user-container">
+        <img src={userlogin} alt="User Icon" className="user-icon" />
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
